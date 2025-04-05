53410409c7c8d177b5d63db1a6c4d858ace990a8
yashvardhan <yashvardhansinhjadeja187@gmail.com>
2025-04-05 18:43:32 +0530

    Update main.cpp
    Add withdraw method for handling account withdrawals

--- main.cpp
+++ main.cpp
@@ -30,6 +30,56 @@ public:
             cout << "Amount must be greater than 0" << endl;
         }
     }
+#include <iostream>
+#include <string>
+using namespace std;
+class BankAccount
+{
+    string accountHolderName;
+    int accountNumber;
+    double balance;
+
+public:
+    BankAccount(string name, int accNum, float bal)
+    {
+        name = accountHolderName;
+        accNum = accountNumber;
+        bal = balance;
+    }
+    ~BankAccount()
+    {
+        cout << "Destructor called" << endl;
+    }
+    void Deposit(float amount)
+    {
+        if (amount > 0)
+        {
+            balance += amount;
+            cout << "Current balance is " << balance << endl;
+        }
+        else
+        {
+            cout << "Amount must be greater than 0" << endl;
+        }
+    }
+};
+int main()
+{
+    string name;
+    int accNum;
+    double bal;
+
+    cout << "Enter Account Holder's name :";
+    getline(cin, name);
+    cout << "Enter Account number :";
+    cin >> accNum;
+    cout << "Enter Initial Balance : $";
+    cin >> bal;
+
+    BankAccount account(name, accNum, bal);
+    
+    return 0;
+}
 };
 int main()
 {
